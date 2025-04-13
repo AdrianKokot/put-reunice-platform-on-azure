9708d1f9d0cd0a9f9f337fc65e8795c182d86011
Adrian Kokot <kokotadriann@gmail.com>
2025-04-13 18:02:27 +0200

    feat: add terraform for creating postgresql
    Fixes #13
    
    Fixes #11

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,6 +11,23 @@ output "storage_sas_token" {
   sensitive = true
 }
 
+output "postgres_server_fqdn" {
+  value = azurerm_postgresql_flexible_server.postgres.fqdn
+}
+
+output "postgres_admin_username" {
+  value = "${var.postgres_user}"
+}
+
+output "postgres_admin_password" {
+  value     = random_password.postgres_password.result
+  sensitive = true
+}
+
+output "postgres_database_name" {
+  value = azurerm_postgresql_flexible_server_database.database.name
+}
+
 output "env_file_path" {
   value = local_file.env_file.filename
 }
